902943a095b503c522e4ab530a4dcd10aff002a0
Aymeric <aymeric.pilaert@hotmail.fr>
2023-06-30 12:41:55 +0200

    Setting up navigation

--- TriviaGameDemo/Views/QuestionView.swift
+++ TriviaGameDemo/Views/QuestionView.swift
@@ -39,6 +39,7 @@ struct QuestionView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(red: 0.984313725490196, green:
                             0.9294117647058824, blue: 0.8470588235294118))
+        .toolbar(.hidden)
     }
 }
 
