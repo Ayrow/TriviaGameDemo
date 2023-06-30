db34327fc20460cc9741c1bc29b1edb10d9d725d
Aymeric <aymeric.pilaert@hotmail.fr>
2023-06-30 12:06:30 +0200

    Initial Commit

---
+++ TriviaGameDemo/TriviaGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameDemoApp.swift
+//  TriviaGameDemo
+//
+//  Created by Aymeric Pilaert on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
